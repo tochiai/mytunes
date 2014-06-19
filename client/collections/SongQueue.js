9a1fa7f6439764ddeac33bd4a5856a61581e2a66
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-06-19 10:15:37 -0700

    Implements dequeueing, adding, play behavior

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,24 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
-
-  initialize: function(){
-  }
-
+   initialize: function(){
+   this.on('ended', function(song){
+      console.log(song.get('url'));
+     this.remove(song);
+   });
+   this.on('dequeue', function(song){
+     this.remove(song);
+   });
+ },
+ add: function(song){
+   Backbone.Collection.prototype.add.call(this, song);
+   if(this.size() === 1){
+     this.playFirst();
+   }
+ },
+ playFirst: function(){
+   this.at(0).play();
+ },
+ dequeue: function(){
+   return this.shift();
+ }
 });
--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,12 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function(songData) {
+    _.each(songData, function(data) {
+      this.add(new this.model(data));
+    }, this);
+  }
 
 });
